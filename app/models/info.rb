5ccea3e023663764c6e5181de4d88aa7a2b954ee
yudino <yohane.udino@gmail.com>
2021-07-11 13:50:45 +0200

    Ajout des champs requis pour mise en db pour info et message

--- app/models/info.rb
+++ app/models/info.rb
@@ -1,3 +1,6 @@
 class Info < ApplicationRecord
+    validates :firstname, presence: true
+    validates :lastname, presence: true
+    validates :email, presence: true
     has_one :message
 end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,4 @@
 class Message < ApplicationRecord
   belongs_to :info
+  validates :review, presence: true
 end
